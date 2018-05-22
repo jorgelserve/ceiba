f8998e8190d953c68b158b6e93ecac78fae5ed4f
Jorge Luis Serna <jorgelserve@icloud.com>
2018-05-22 14:38:58 -0400

    front end done

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require("express")
 const app = express()
 
 var v_carga;
+var v_panel;
+var v_baterias;
 
 app.get("/", (req, res) => {
-	res.send("Hola mundo")
+  var template = `<center>
+    Voltaje carga: ${v_carga} <br>
+    Voltaje panel: ${v_panel} <br>
+    Voltaje baterias: ${v_baterias} <br>
+  </center>`
+	res.send()
 })
 
 app.post("/api", (req, res) => {
-	v_carga = req.body.v_carga
-	v_panel = req.body.v_pane√lq 
+	v_carga = req.body.v_carga || null
+	v_panel = req.body.v_panel || null
+  v_baterias = req.body.v_baterias || null
+  res.send("OK")
 })
 
 var port = process.env.PORT || 5000
